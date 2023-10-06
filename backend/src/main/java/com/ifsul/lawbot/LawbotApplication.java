d11a386bc9b71485fcdadf971258b1b3288467a3
Érick Goulardt <erick.goulardt2004@gmail.com>
2023-10-06 15:04:03 -0300

    Primeiro commit

---
+++ backend/src/main/java/com/ifsul/lawbot/LawbotApplication.java
@@ -0,0 +1,13 @@
+package com.ifsul.lawbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawbotApplication.class, args);
+	}
+
+}

8440b0b678bb2de359bc34636524a1e3b11d795d
João Fernandes <jguiborges@gmail.com>
2023-10-17 08:47:54 -0300

    Login advogado feito

--- backend/src/main/java/com/ifsul/lawbot/services/AdvogadoService.java
+++ backend/src/main/java/com/ifsul/lawbot/services/AdvogadoService.java
@@ -20,6 +20,9 @@ import java.security.PrivateKey;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ifsul.lawbot.services.CriptografiaService.decriptar;
+import static com.ifsul.lawbot.services.CriptografiaService.encriptar;
+
 @Service
 public class AdvogadoService {
 
@@ -29,24 +32,22 @@ public class AdvogadoService {
     @Autowired
     private GerarChaveService gerarChaveService;
 
-    @Autowired
-    private CriptografiaService cript;
 
     public ResponseEntity cadastrarAdvogado(CadastrarAdvogadoRequest dados, UriComponentsBuilder uriBuilder){
         Advogado advogado = Advogado.builder().dataNascimento(dados.dataNascimento()).senha(HashSenhasService.hash(dados.senha())).build();
 
         Chave key = gerarChaveService.findKey();
         advogado.setCpf(
-                cript.encriptar(dados.cpf(), key.getChavePublica())
+                encriptar(dados.cpf(), key.getChavePublica())
         );
         advogado.setNome(
-                cript.encriptar(dados.nome(), key.getChavePublica())
+                encriptar(dados.nome(), key.getChavePublica())
         );
         advogado.setEmail(
-                cript.encriptar(dados.email(), key.getChavePublica())
+                encriptar(dados.email(), key.getChavePublica())
         );
         advogado.setOab(
-                cript.encriptar(dados.oab(), key.getChavePublica())
+                encriptar(dados.oab(), key.getChavePublica())
         );
         advogado.setChave(key);
 
@@ -61,10 +62,10 @@ public class AdvogadoService {
         List<ListarAdvogadoRequest> advogadosDecriptado = new ArrayList<>();
         List<Advogado> advogados = repository.findAll().stream().map(
                 advogado -> {
-                    advogado.setNome(cript.decriptar(advogado.getNome(), advogado.getChave().getChavePrivada()));
-                    advogado.setCpf(cript.decriptar(advogado.getCpf(), advogado.getChave().getChavePrivada()));
-                    advogado.setOab(cript.decriptar(advogado.getOab(), advogado.getChave().getChavePrivada()));
-                    advogado.setEmail(cript.decriptar(advogado.getEmail(), advogado.getChave().getChavePrivada()));
+                    advogado.setNome(decriptar(advogado.getNome(), advogado.getChave().getChavePrivada()));
+                    advogado.setCpf(decriptar(advogado.getCpf(), advogado.getChave().getChavePrivada()));
+                    advogado.setOab(decriptar(advogado.getOab(), advogado.getChave().getChavePrivada()));
+                    advogado.setEmail(decriptar(advogado.getEmail(), advogado.getChave().getChavePrivada()));
 
                     advogadosDecriptado.add(new ListarAdvogadoRequest(advogado));
                     return advogado;
@@ -81,10 +82,10 @@ public class AdvogadoService {
             advogado.setDataNascimento(dados.dataNascimento());
         }
         if( dados.email() != null){
-            advogado.setEmail(cript.encriptar(dados.email(), advogado.getChave().getChavePublica()));
+            advogado.setEmail(encriptar(dados.email(), advogado.getChave().getChavePublica()));
         }
         if( dados.nome() != null){
-            advogado.setNome(cript.encriptar(dados.nome(), advogado.getChave().getChavePublica()));
+            advogado.setNome(encriptar(dados.nome(), advogado.getChave().getChavePublica()));
         }
 
         return ResponseEntity.ok(new DetalharAdvogadoRequest(advogado));
@@ -101,10 +102,10 @@ public class AdvogadoService {
         var advogado = repository.getReferenceById(id);
         var advogadoDecriptografado = new Advogado();
         advogadoDecriptografado.setId(id);
-        advogadoDecriptografado.setNome(cript.decriptar(advogado.getNome(), advogado.getChave().getChavePrivada()));
-        advogadoDecriptografado.setCpf(cript.decriptar(advogado.getCpf(), advogado.getChave().getChavePrivada()));
-        advogadoDecriptografado.setOab(cript.decriptar(advogado.getOab(), advogado.getChave().getChavePrivada()));
-        advogadoDecriptografado.setEmail(cript.decriptar(advogado.getEmail(), advogado.getChave().getChavePrivada()));
+        advogadoDecriptografado.setNome(decriptar(advogado.getNome(), advogado.getChave().getChavePrivada()));
+        advogadoDecriptografado.setCpf(decriptar(advogado.getCpf(), advogado.getChave().getChavePrivada()));
+        advogadoDecriptografado.setOab(decriptar(advogado.getOab(), advogado.getChave().getChavePrivada()));
+        advogadoDecriptografado.setEmail(decriptar(advogado.getEmail(), advogado.getChave().getChavePrivada()));
         advogadoDecriptografado.setDataNascimento(advogado.getDataNascimento());
 
         return ResponseEntity.ok(new DetalharAdvogadoRequest(advogadoDecriptografado));
--- backend/src/main/java/com/ifsul/lawbot/services/AutenticacaoService.java
+++ backend/src/main/java/com/ifsul/lawbot/services/AutenticacaoService.java
@@ -1,17 +1,17 @@
 package com.ifsul.lawbot.services;
 
 import com.ifsul.lawbot.entities.Advogado;
-import com.ifsul.lawbot.entities.Chave;
 import com.ifsul.lawbot.repositories.AdvogadoRepository;
-import com.ifsul.lawbot.repositories.ChaveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.security.PublicKey;
 import java.util.List;
+import java.util.Objects;
+
+import static com.ifsul.lawbot.services.CriptografiaService.decriptar;
 
 @Service
 public class AutenticacaoService implements UserDetailsService {
@@ -19,16 +19,13 @@ public class AutenticacaoService implements UserDetailsService {
     @Autowired
     private AdvogadoRepository repository;
 
-    @Autowired
-    private CriptografiaService cript;
-
-    @Autowired
-    ChaveRepository chaveRepository;
-
     @Override
     public UserDetails loadUserByUsername(String oab) throws UsernameNotFoundException {
-        PublicKey chave = chaveRepository.findById(1L).get().getChavePublica();
-        System.out.println(chave.toString());
-        return repository.findByOab(cript.encriptar(oab, chave));
+
+        Advogado adv = repository.findAll().stream().filter(advogado ->
+                Objects.equals(decriptar(advogado.getOab(), advogado.getChave().getChavePrivada()), oab)
+        ).toList().stream().findFirst().get();
+
+        return adv;
     }
 }
